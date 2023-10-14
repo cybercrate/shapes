e1c66294b41cac20686b789da33dfe590d5d4b87
Alexander Shavrov <alexander.shavrov@mail.ru>
2023-10-14 10:46:01 +0300

    Update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,14 +14,8 @@ repositories {
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     // This dependency is used by the application.
     implementation("com.google.guava:guava:32.1.1-jre")
-
-    implementation("com.google.zxing:core:3.5.2")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -34,11 +28,6 @@ java {
 application {
     // Define the main class for the application.
     mainClass.set("com.wingmann.shapes.Main")
-}
-
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
 }
 
 tasks.jar {
